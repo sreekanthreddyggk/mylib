62522788beb96a7cacb2aefa9459024bce9ad492
sreekanthreddyggk <40565296+sreekanthreddyggk@users.noreply.github.com>
2018-11-19 14:18:23 +0530

    Update testfile.groovy

--- vars/testfile.groovy
+++ vars/testfile.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-   powershell returnStatus: true, script: '.\\mylib\\vars\\sc.ps1'
+   powershell returnStatus: true, script: '.\\sc.ps1'
    var0 = "working"
    this.var1 = "'not working'"
    this.var2 = "'devops working'"
