0902496667e4b72f87106a7d4fcad88406eccbd5
sreekanthreddyggk <40565296+sreekanthreddyggk@users.noreply.github.com>
2018-11-19 12:39:13 +0530

    Update testfile.groovy

--- vars/testfile.groovy
+++ vars/testfile.groovy
@@ -8,7 +8,7 @@ def getFoo()  {
 def call() {
    this.foo = "working"
    this.var1 = "'not working'"
-   this.git_url= "it credentialsId: 'e8e06f69-3c5e-4417-a719-ad4e73f6b346', url: 'https://github.com/sreekanthreddyggk/ASPNETCore-WebAPI-Sample.git'"
+   this.var2 = "'devops working'"
 
 
 }
