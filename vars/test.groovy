2fc4b589ccb12c5376334dc32f0e3473c50e5eb8
sreekanthreddyggk <40565296+sreekanthreddyggk@users.noreply.github.com>
2018-11-20 15:14:21 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,7 +2,7 @@ def call() {
    powershell '''
      Write-Output 'HIE devops'
    
-    C:\\'Program Files (x86)'\\Jenkins\\workspace\\multi-share@libs\\mytestlibrary\\vars\\hello-world.ps1
+    .\\hello-world.ps1
     '''
 
 
