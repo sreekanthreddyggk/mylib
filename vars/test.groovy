dc719327a7f78e4c4b0bf42b0341d082619ffd24
sreekanthreddyggk <40565296+sreekanthreddyggk@users.noreply.github.com>
2018-11-20 14:05:16 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,7 +2,7 @@ def call() {
    powershell '''
      Write-Output 'HIE devops
    
-    powershellLibraryScript resource: . '\\sc.ps1'
+    powershellLibraryScript resource: . "\\sc.ps1"
     '''
 
 
